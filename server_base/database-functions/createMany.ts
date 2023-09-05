620c113f3f26f03aca95c45df56e77287df56b30
linh.tq <tranquanglinh.pt@gmail.com>
2023-09-05 19:57:26 +0700

    Commit code TODO: Nghiên cứu cách tự sinh ra view để truy vấn thông qua aggregate

--- server_base/database-functions/createMany.ts
+++ server_base/database-functions/createMany.ts
@@ -1,5 +1,18 @@
-import { Model } from "mongoose";
+import {SCHEMA_TYPE} from "../../schemas/SchemaTypes";
+import {DATABASE_MODELS} from "../mongoose/DatabaseModels";
+import {SCHEMAS_CONFIG} from "../../share/schema_configs";
+import {zTempFileId} from "../zodUtils";
+import {getTempFiles} from "../file-storage/FileManager";
 
-export async function createMany(input: any, Model: Model<any>) {
-  return Model.insertMany(input) as unknown as string[];
+export async function createMany(input: any, schema: SCHEMA_TYPE) {
+    const Model = DATABASE_MODELS[schema];
+    const SchemaConfig = SCHEMAS_CONFIG[schema];
+    SchemaConfig.fileTypeKeys.forEach(key => {
+        if (input[key] && zTempFileId().parse(input[key])) {
+            const file = getTempFiles(input[key])
+            //TODO kiểm tra xem đã tự lưu file vào DB hay chưa -> chưa thì lưu vào DB và thay tempID = ID trong DB
+            //TODO tạo hàm để gọi lại + nếu tempFile đã bị xoá => Phải huỷ request.
+        }
+    })
+    return Model.insertMany(input) as unknown as string[];
 }
--- share/schema_configs/index.ts
+++ share/schema_configs/index.ts
@@ -1,2 +1,5 @@
-export const SCHEMAS_CONFIG = {
-};
+// This file is template only, will be auto gen
+import {SCHEMA_TYPE} from "../../schemas/SchemaTypes";
+import {ISchemaConfig} from "../types/ISchemaConfig";
+
+export const SCHEMAS_CONFIG: Record<SCHEMA_TYPE, ISchemaConfig<any>> = {} as any;
