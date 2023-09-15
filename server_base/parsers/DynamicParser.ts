70926eeeb75dab6ecc7b9cf4dbd400c9583803ad
linh.tq <tranquanglinh.pt@gmail.com>
2023-09-15 19:40:43 +0700

    Update schema,
    Update Dynamic Parser, tự động bỏ qua các trường hợp không cần thiết gây truy vấn lỗi (không có unique key, không có possible value)

--- server_base/parsers/DynamicParser.ts
+++ server_base/parsers/DynamicParser.ts
@@ -26,10 +26,10 @@ function safeParseObjectID(id: any) {
 }
 
 function getUniqueRefValues<T>(
-  records: T[],
-  staticKeys: (keyof T)[],
-  dynamicKeys: (keyof T)[],
-  SchemaConfig: ISchemaConfig<T>,
+    records: T[],
+    staticKeys: (keyof T)[],
+    dynamicKeys: (keyof T)[],
+    SchemaConfig: ISchemaConfig<T>,
 ) {
   const StaticRefValues = new Map(staticKeys.map((k) => [k, new Set()]));
   const DynamicRefValues = new Map(dynamicKeys.map((k) => [k, new Set()]));
@@ -41,10 +41,10 @@ function getUniqueRefValues<T>(
       const categoryField = getCategoryKeyOfDynamicData(field);
       if (data[field] && data[categoryField])
         DynamicRefValues.get(field)?.add(
-          JSON.stringify({
-            value: data[field],
-            category: data[categoryField],
-          } as DynamicValue),
+            JSON.stringify({
+              value: data[field],
+              category: data[categoryField],
+            } as DynamicValue),
         );
     });
   });
@@ -52,13 +52,13 @@ function getUniqueRefValues<T>(
 }
 
 export async function getSchemaDataFromArray<T>(
-  records: T[],
-  SchemaConfig: ISchemaConfig<T>,
-  InputSchema: ZodTypeAny,
+    records: T[],
+    SchemaConfig: ISchemaConfig<T>,
+    InputSchema: ZodTypeAny,
 ) {
   const [dynamicKeys, staticKeys] = _.partition(
-    SchemaConfig.relationKeys,
-    (key) => isDynamicSchemaType(SchemaConfig.fieldConfigs[key].type),
+      SchemaConfig.relationKeys,
+      (key) => isDynamicSchemaType(SchemaConfig.fieldConfigs[key].type),
   );
 
   console.log("Dynamic Keys:", dynamicKeys);
@@ -68,25 +68,29 @@ export async function getSchemaDataFromArray<T>(
    * lấy ra giá trị JSON.stringify({categoryName, uniqueValue})
    */
   const { DynamicRefValues, StaticRefValues } = getUniqueRefValues(
-    records,
-    staticKeys,
-    dynamicKeys,
-    SchemaConfig,
+      records,
+      staticKeys,
+      dynamicKeys,
+      SchemaConfig,
   );
+
+  console.log("Static REf", StaticRefValues);
 
   /** Step 1A: Đối với dynamicKey: tạo Mapping JSON.stringify({categoryName, uniqueValue}) => _id */
   const DynamicRefMap = await getDynamicRefMap(
-    SchemaConfig,
-    dynamicKeys,
-    DynamicRefValues,
+      SchemaConfig,
+      dynamicKeys,
+      DynamicRefValues,
   );
 
   /** Step 1B: Đối với staticKey: tạo Mapping uniqueValue => _id  */
   const StaticRefMap = await getStaticRefMap(
-    SchemaConfig,
-    staticKeys,
-    StaticRefValues,
+      SchemaConfig,
+      staticKeys,
+      StaticRefValues,
   );
+
+  console.log("Static ref map", StaticRefMap);
 
   /** Step 2: Mapping RefValue => ObjectID   */
   const verifiedRecords: T[] = [];
@@ -106,6 +110,7 @@ export async function getSchemaDataFromArray<T>(
       data[k] = DynamicRefMap.get(k)?.get(mapKey) ?? safeParseObjectID(data[k]);
     });
     //2B: Verify with Zod
+    console.log("Data before parse zod", data);
     const parseResult = verifyWithZod(InputSchema, data);
     if (parseResult.success) {
       verifiedRecords.push(parseResult.data as any);
@@ -116,9 +121,9 @@ export async function getSchemaDataFromArray<T>(
         errors: parseResult.errors.map((error) => ({
           ...error,
           path:
-            SchemaConfig.fieldConfigs[
-              error.path as keyof typeof SchemaConfig.fieldConfigs
-            ]?.label ?? error.path,
+              SchemaConfig.fieldConfigs[
+                  error.path as keyof typeof SchemaConfig.fieldConfigs
+                  ]?.label ?? error.path,
         })),
       });
     }
@@ -138,22 +143,22 @@ export async function getAllUniqueKeysByCategory(config: DynamicConfig) {
     $joinTable("_id", config.property, "properties", DYNAMIC_CATEGORY_ID),
   ]);
   const result: Record<string, string[]> = categoryInfos.reduce(
-    (acc, catInfo) => ({
-      ...acc,
-      [catInfo[DYNAMIC_CATEGORY_ID]]: catInfo["properties"]
+      (acc, catInfo) => ({
+        ...acc,
+        [catInfo[DYNAMIC_CATEGORY_ID]]: catInfo["properties"]
         .filter((p: ISchemaFieldConfig) => p.unique)
         .map((p: any) => String(p._id)),
-    }),
-    {},
+      }),
+      {},
   );
   console.log("uniqueKey of Dynamic categories:", result);
   return result;
 }
 
 async function getDynamicRefMap<T>(
-  SchemaConfig: ISchemaConfig<T>,
-  dynamicKeys: (keyof T)[],
-  DynamicRefValues: Map<keyof T, Set<any>>,
+    SchemaConfig: ISchemaConfig<T>,
+    dynamicKeys: (keyof T)[],
+    DynamicRefValues: Map<keyof T, Set<any>>,
 ) {
   const RefMap = new Map(dynamicKeys.map((k) => [k, new Map()]));
   for (let i = 0; i < dynamicKeys.length; i++) {
@@ -161,48 +166,49 @@ async function getDynamicRefMap<T>(
     const possibleValueSet = DynamicRefValues.get(refKey);
     if (!possibleValueSet) continue;
     const rawPossibleValues = Array.from(possibleValueSet.values());
+    if(!rawPossibleValues.length) continue;
     const possibleValues = rawPossibleValues.map((v) =>
-      JSON.parse(v),
+        JSON.parse(v),
     ) as DynamicValue[];
     const refSchemaType = getSingleType<SCHEMA_TYPE>(
-      SchemaConfig.fieldConfigs[refKey].type,
+        SchemaConfig.fieldConfigs[refKey].type,
     );
     const dynamicConfig = SCHEMAS_CONFIG[refSchemaType].dynamic as any; // chắc chắn có
     const UNIQUE_KEYS = await getAllUniqueKeysByCategory(dynamicConfig);
     //Build query dựa vào các giá trị có + Schema config
     const candidates = await DATABASE_MODELS[refSchemaType].aggregate(
-      _.compact([
-        ...$manyToOneJoin(DYNAMIC_CATEGORY_ID, dynamicConfig.category),
-        {
-          $set: {
-            [DYNAMIC_CATEGORY_ID]: `$${DYNAMIC_CATEGORY_ID}.${DYNAMIC_CATEGORY_ID}`,
+        _.compact([
+          ...$manyToOneJoin(DYNAMIC_CATEGORY_ID, dynamicConfig.category),
+          {
+            $set: {
+              [DYNAMIC_CATEGORY_ID]: `$${DYNAMIC_CATEGORY_ID}.${DYNAMIC_CATEGORY_ID}`,
+            },
           },
-        },
-        {
-          $match: {
-            $or: possibleValues.map((v) => ({
-              $and: [
-                { [DYNAMIC_CATEGORY_ID]: v.category },
-                {
-                  $or: UNIQUE_KEYS[v.category].map((key) => ({
-                    [key]: v.value,
-                  })),
-                },
-              ],
-            })),
+          {
+            $match: {
+              $or: possibleValues.map((v) => ({
+                $and: [
+                  { [DYNAMIC_CATEGORY_ID]: v.category },
+                  {
+                    $or: UNIQUE_KEYS[v.category].map((key) => ({
+                      [key]: v.value,
+                    })),
+                  },
+                ],
+              })),
+            },
           },
-        },
-      ]),
+        ]),
     );
 
     console.log("Candidates", candidates);
 
     candidates.forEach((can: any) => {
       console.log(
-        "Unique key of",
-        can,
-        ":======",
-        UNIQUE_KEYS[can[DYNAMIC_CATEGORY_ID]],
+          "Unique key of",
+          can,
+          ":======",
+          UNIQUE_KEYS[can[DYNAMIC_CATEGORY_ID]],
       );
       UNIQUE_KEYS[can[DYNAMIC_CATEGORY_ID]].forEach((key) => {
         console.log("Raw possible", rawPossibleValues);
@@ -226,9 +232,9 @@ async function getDynamicRefMap<T>(
 }
 
 async function getStaticRefMap<T>(
-  SchemaConfig: ISchemaConfig<T>,
-  staticKeys: (keyof T)[],
-  StaticRefValues: Map<keyof T, Set<any>>,
+    SchemaConfig: ISchemaConfig<T>,
+    staticKeys: (keyof T)[],
+    StaticRefValues: Map<keyof T, Set<any>>,
 ) {
   const RefMap = new Map(staticKeys.map((k) => [k, new Map()]));
   for (let i = 0; i < staticKeys.length; i++) {
@@ -236,11 +242,14 @@ async function getStaticRefMap<T>(
     const possibleValueSet = StaticRefValues.get(refKey);
     if (!possibleValueSet) continue;
     const possibleValues = Array.from(possibleValueSet.values());
+    if (!possibleValues.length) continue;
     const refSchemaType = getSingleType<SCHEMA_TYPE>(
-      SchemaConfig.fieldConfigs[refKey].type,
+        SchemaConfig.fieldConfigs[refKey].type,
     );
     //Build query dựa vào các giá trị có + Schema config
     const UNIQUE_KEYS = SCHEMAS_CONFIG[refSchemaType].uniqueKeys;
+    if(!UNIQUE_KEYS.length) continue;
+    console.log("Unique keys", UNIQUE_KEYS);
     const candidates = await DATABASE_MODELS[refSchemaType].aggregate([
       {
         $match: {
@@ -261,5 +270,6 @@ async function getStaticRefMap<T>(
       });
     });
   }
+  console.log("Static RefMap", RefMap);
   return RefMap;
 }
